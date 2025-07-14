7f30feb312684835a2774a84caaaf194e2b14bdd
Scorti-Paul <scortip@gmail.com>
2025-07-14 15:37:23 +0000

    Add user routes with sign-up and sign-in functionality

---
+++ src/routes/user/index.ts
@@ -0,0 +1,29 @@
+import {
+  createUser,
+  signIn
+  // deleteUser,
+  // getUsers,
+  // getUserById,
+  // updateUser,
+} from "../../controllers/user";
+// import { UserModel } from "../../database/schema/user";
+
+const express = require("express");
+
+const router = express?.Router();
+
+    // router.get("/api/users", (req: any, res: any) =>
+    //   getUsers(UserModel, req, res)
+    // );
+
+    // router.get("/api/user", (req: any, res: any) =>
+    //   getUserById(UserModel, req, res)
+    // );
+
+    // router.put("/api/user/:id", updateUser);
+router.post("/api/user/sign-up", createUser);
+router.post("/api/user/sign-in", signIn);
+// router.delete("/api/user/:id", deleteUser);
+
+const userRoutes = router;
+export { userRoutes };
