b472fea593c40c95ebf90b0746e49403ea35a467
Scorti-Paul <scortip@gmail.com>
2025-07-14 15:35:43 +0000

    Add tag schema and model for MongoDB using Mongoose

---
+++ src/database/schema/tag/index.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const TagSchema = new mongoose.Schema(
+  {
+    name: {
+      type: mongoose.SchemaTypes.String,
+      required: [true, "Tag name is required"],
+      unique: [true, "Tag name must be unique"],
+    },
+    slug: {
+      type: mongoose.SchemaTypes.String,
+      required: [true, "Tag slug is required"],
+      unique: [true, "Tag slug must be unique"],
+    },
+    description: {
+      type: mongoose.SchemaTypes.String,
+    },
+    parentCategory: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Category",
+    },
+    image: { type: mongoose.SchemaTypes.String, required: false, default: "" },
+    isActive: { type: mongoose.SchemaTypes.Boolean, default: true },
+  },
+  {
+    toJSON: {
+      transform(_: any, data: any) {
+        delete data.__v;
+      },
+    },
+    timestamps: true,
+  }
+);
+export const TagModel = mongoose.model("Tag", TagSchema);
