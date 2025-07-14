4c995dfb9e261cdfe4f103f5d2b063a3121677d0
Scorti-Paul <scortip@gmail.com>
2025-07-14 15:33:55 +0000

    Add category schema and model for MongoDB using Mongoose

---
+++ src/database/schema/category/index.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: mongoose.SchemaTypes.String,
+      required: [true, "Category name is required"],
+      unique: [true, "Category name must be unique"],
+    },
+    type: {
+      type: mongoose.SchemaTypes.String,
+      required: [true, "Category type is required"],
+      unique: [true, "Category type must be unique"],
+    },
+    slug: {
+      type: mongoose.SchemaTypes.String,
+      required: [true, "Category slug is required"],
+      unique: [true, "Category slug must be unique"],
+    },
+    description: {
+      type: mongoose.SchemaTypes.String,
+    },
+    isActive: { type: mongoose.SchemaTypes.Boolean, default: true },
+    image: { type: mongoose.SchemaTypes.String, required: false, default: "" },
+  },
+  {
+    toJSON: {
+      transform(_: any, data: any) {
+        delete data.__v;
+      },
+    },
+    timestamps: true,
+  }
+);
+export const CategoryModel = mongoose.model("Category", CategorySchema);
